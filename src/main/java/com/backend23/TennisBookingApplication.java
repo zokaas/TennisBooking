f73b1917bf8fe6f79fb1e6af8249e1bbfac69f3f
zokaas <bozic.zorana@gmail.com>
2023-10-31 14:40:01 +0200

    Initial commit

---
+++ src/main/java/com/backend23/TennisBookingApplication.java
@@ -0,0 +1,13 @@
+package com.backend23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisBookingApplication.class, args);
+	}
+
+}
